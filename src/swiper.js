35cc9f4b87fc7951953414efea9a6cef2cef9f0c
viss5vuus98 <dokipu@gmailcom>
2023-08-22 00:41:59 +0800

    新增登入頁, 結帳頁

--- src/swiper.js
+++ src/swiper.js
@@ -1,11 +1,24 @@
-import Swiper from "swiper";
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
 
-const swiper = new Swiper('.marqueeContent', {
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.marquee', {
   allowTouchMove: false,
   slidesPerView: 'auto',
   autoplay: {
     delay: 0
   },
   loop: true,
-  speed: 5000,
+  speed: 8000,
+})
+
+const swiper2 = new Swiper('.carousel', {
+  slidesPerView: 1,
+  spaceBetween: 15,  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
 })
